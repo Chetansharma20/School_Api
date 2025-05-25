31d0d32cda1e2fde138def22fb1a51f420f61830
Chetansharma20 <sharmachetan20082000@gmail.com>
2025-05-25 14:47:03 +0530

    M

--- src/models/Database.js
+++ src/models/Database.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || sql12.freesqldatabase.com ,
+  user: process.env.DB_USER ||sql12781076 ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME || sql12781076 ,
    port: process.env.DB_PORT || 3306
 });
 

8a002bc4e83cd65f63cfe20264ac043ffa872379
Chetansharma20 <sharmachetan20082000@gmail.com>
2025-05-25 13:13:15 +0530

    M

--- src/models/Database.js
+++ src/models/Database.js
@@ -1,13 +1,13 @@
 // db.js
 const mysql = require('mysql2');
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = mysql.createConnection({
-  host:  'localhost',
-  user:  'root',
-  password: 'Chetan20@',
-  database: 'school_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {

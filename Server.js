49cdcb279912f5add640b759a73447ff58d54150
Chetansharma20 <sharmachetan20082000@gmail.com>
2025-05-25 11:53:52 +0530

    M

--- Server.js
+++ Server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const schoolRoutes = require('./src/routes/schoolRoutes')
 const app = express()
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to School Management API. Use /api/addSchool and /api/listSchools endpoints.');
+});
+
+
 app.use('/api', schoolRoutes);
 
 app.listen(5000,()=>

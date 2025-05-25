166078935c397aa95fd53aa7e367535628e429ed
Chetansharma20 <sharmachetan20082000@gmail.com>
2025-05-25 16:05:14 +0530

    M

--- src/models/Database.js
+++ src/models/Database.js
@@ -3,15 +3,16 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST || sql12.freesqldatabase.com ,
   user: process.env.DB_USER ||sql12781076 ,
   password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME || sql12781076 ,
-   port: process.env.DB_PORT || 3306
+   port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
 });
 
-db.connect((err) => {
+pool.getConnection((err) => {
   if (err) {
     console.error('Database connection failed:', err.stack);
     return;
@@ -19,4 +20,4 @@ db.connect((err) => {
   console.log('Connected to MySQL');
 });
 
-module.exports = db;
+module.exports = pool;
